9c20e0fc08b49658cc3d18a6042279c8d2a6b3f3
MHammadYou <MHammadY2.0@gmail.com>
2021-11-23 23:38:02 +0500

    Added templates responses

--- src/routes/users/login.ts
+++ src/routes/users/login.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+const fs = require('fs').promises;
 
 
 const router = Router();
 
 
-router.get('/login', (req, res) => {
-  res.json({"route": "/login"})
+router.get('/login', async (req, res) => {
+  res.send( await fs.readFile('./public/login.html', 'utf-8') );
 })
 
 export default router;
--- src/routes/users/signup.ts
+++ src/routes/users/signup.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+const fs = require('fs').promises;
 
 
 const router = Router();
 
 
-router.get('/signup', (req, res) => {
-  res.json({"route": "/signup"})
+router.get('/signup', async (req, res) => {
+  res.send( await fs.readFile('./public/signup.html', 'utf-8') );
 })
 
 export default router;
