1c003e2ee3d6aee8a5499117d8622b9d156dadb1
MHammadYou <MHammadY2.0@gmail.com>
2021-11-25 23:53:59 +0500

    Added Ads schema

--- src/models/ads.ts
+++ src/models/ads.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
+import Advertisers from "./advertisers";
 
 
-const schema = new mongoose.Schema(
+const adsSchema = new mongoose.Schema(
   {
-
+    path: String,
+    advertiser: Advertisers,
+    views: Number,
+    datetime: { type: Date, default: new Date()}
   }
 )
 
-const Ads = mongoose.model('Ads', schema);
+const Ads = mongoose.model('Ads', adsSchema);
 
 export default Ads;
---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+export { default as Ads } from "./ads";
+export { default as Advertisers } from "./advertisers";
+export { default as Publishers } from "./publishers";
+export { default as Users } from "./users";
