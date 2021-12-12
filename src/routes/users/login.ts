65de923f04484eeaf53142506df5099e651e3fbe
MHammadYou <MHammadY2.0@gmail.com>
2021-12-12 23:12:07 +0500

    Refactored code

--- src/routes/users/login.ts
+++ src/routes/users/login.ts
@@ -9,7 +9,7 @@ router.get('/login', async (req, res) => {
   res.send( await fs.readFile('./public/users/login.html', 'utf-8') );
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   res.send("Post req on /login route");
 })
 
--- src/routes/users/signup.ts
+++ src/routes/users/signup.ts
@@ -9,7 +9,7 @@ router.get('/signup', async (req, res) => {
   res.send( await fs.readFile('./public/users/signup.html', 'utf-8') );
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
   res.send("Post req on signup route")
 })
 
