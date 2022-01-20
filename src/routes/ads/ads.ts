5e53469410c4d1a6e9a49866cf1e3e1f30194271
MHammadYou <MHammadY2.0@gmail.com>
2022-01-20 23:47:50 +0500

    Added ads route

--- src/routes/ads/ads.ts
+++ src/routes/ads/ads.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { readFile } from "fs/promises";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send("Default route for ads");
+router.get('/', async (req, res) => {
+  res.send( await readFile('./public/ads/ads.html', 'utf-8') );
 })
 
 router.get('/id', (req, res) => {
