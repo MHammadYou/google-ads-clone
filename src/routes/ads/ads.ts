219291ae31d9092188be84243efa1371a00c4b8f
MHammadYou <MHammadY2.0@gmail.com>
2022-01-22 23:47:38 +0500

    Added create-ad.html

--- src/routes/ads/ads.ts
+++ src/routes/ads/ads.ts
@@ -11,8 +11,8 @@ router.get('/id', (req, res) => {
   res.send("Default route for ad");
 })
 
-router.post('/create', (req, res) => {
-  res.send("create");
+router.post('/create', async (req, res) => {
+  res.send( await readFile('./public/ads/create-ad.html', 'utf-8') );
 })
 
 router.post('/update/id', (req, res) => {
