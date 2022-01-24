0f48a721be93023f8a76033e3d90d83e62589fc5
MHammadYou <MHammadY2.0@gmail.com>
2022-01-24 23:27:27 +0500

    Refactored code

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
 import { readFile } from "fs/promises";
 
-
 const router = Router();
-
 
 router.get('/', async (req, res) => {
   res.send( await readFile('./public/index.html', 'utf-8') );
