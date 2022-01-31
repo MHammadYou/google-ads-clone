8e02ef806ffd20cbcdf9d472d6c096357167513b
MHammadYou <MHammadY2.0@gmail.com>
2022-01-31 23:32:38 +0500

    Added tailwind css

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,17 @@ app.set('view engine', 'ejs');
 //   .catch(err => console.log(err));
 
 app.use(express.json());
+app.use(express.static(__dirname + 'static'));
 
 app.use('/', homeRoute);
-app.use('/', signupRoute);
-app.use('/', loginRoute);
+app.use('/users/', signupRoute);
+app.use('/users/', loginRoute);
 app.use('/', contentRoute);
 app.use('/ads', adsRoutes);
+
+app.use((req, res) => {
+  res.status(404).send("No content on this route")
+})
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
