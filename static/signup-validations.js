5cca0fefb2f9edeb19e802111ff07b507d7609ff
MHammadYou <MHammadY2.0@gmail.com>
2022-03-07 23:51:59 +0500

    Added validate email method in signup validations

--- static/signup-validations.js
+++ static/signup-validations.js
@@ -1,1 +1,11 @@
-console.log("Hit")
+const validateEmail = inputEmail => {
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputEmail.value.match(mailFormat)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
