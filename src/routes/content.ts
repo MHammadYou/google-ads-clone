abe1152f00b0a02cec6c2dc0ff9423edf51c636b
MHammadYou <MHammadY2.0@gmail.com>
2022-03-20 23:46:27 +0500

    Added basic ad content route

--- src/routes/content.ts
+++ src/routes/content.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import AdsModel from "../models/ads";
 
 const router = Router();
 
 
 router.get('/', (req, res) => {
-  res.send('Content delivering route');
+  const session: any = req.session;
+  const user = session.user;
+
+  if (!user) {
+    res.redirect('/');
+    return;
+  }
+
+  const ad = AdsModel.find();
+  
+  res.send(ad);
 })
 
 export default router;
